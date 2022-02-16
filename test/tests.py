b0b5c870b700ee00b32dc46f9731d0c9c15c6f5d
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-16 15:16:24 +0200

    Run tests in headless mode

--- test/tests.py
+++ test/tests.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-driver = webdriver.Firefox()
+options = FirefoxOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Firefox(options=options)
 driver.get("http://127.0.0.1:5000/")
 
 elem = driver.find_element(By.ID,"hello")
