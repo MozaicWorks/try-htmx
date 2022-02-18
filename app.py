368a3f55026546d0bb018349c80fc57e1dcf1124
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-18 14:09:35 +0200

    Refactor: add container for the list

--- app.py
+++ app.py
@@ -9,22 +9,26 @@ def index():
 @app.route('/lists', methods=['POST'])
 def lists():
     return """
-        <h1 id="first-list-header">First List</h1>
-        <ol id='first-list'>
-            <li>Item 1</li>
-        </ol>
-        <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
+        <div id="first-list-container">
+            <h1 id="first-list-header">First List</h1>
+            <ol id='first-list'>
+                <li>Item 1</li>
+            </ol>
+            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
+        </div>
     """
 
 @app.route('/first-list/add', methods=['POST'])
 def newItem():
     return """
-        <h1 id="first-list-header">First List</h1>
-        <ol id='first-list'>
-            <li>Item 1</li>
-            <li>Item 2</li>
-        </ol>
-        <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
+        <div id="first-list-container">
+            <h1 id="first-list-header">First List</h1>
+            <ol id='first-list'>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ol>
+            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
+        </div>
     """
 
 
