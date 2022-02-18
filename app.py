8e844f700720358e3fa77a928e15a0339c4a126e
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-18 14:51:48 +0200

    Refactor server-side code to allow for removing duplication

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, request
 
 app = Flask(__name__)
 
@@ -13,20 +13,24 @@ def lists():
             <h1 id="first-list-header">First List</h1>
             <ol id='first-list'>
                 <li>Item 1</li>
+                <input name="items[]" type="text" value="Item 1"></input>
             </ol>
-            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container">Add Item</button>
+            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container" hx-include="input">Add Item</button>
         </div>
     """
 
 @app.route('/first-list/add', methods=['POST'])
 def newItem():
+    items = request.form['items[]']
     return """
             <h1 id="first-list-header">First List</h1>
             <ol id='first-list'>
                 <li>Item 1</li>
+                <input name="items[]" type="text" value="Item 1"></input>
                 <li>Item 2</li>
+                <input name="items[]" type="text" value="Item 2"></input>
             </ol>
-            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container">Add Item</button>
+             <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container" hx-include="input">Add Item</button>
     """
 
 
