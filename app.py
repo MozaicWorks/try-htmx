c8011e83f39d0a3473720b34806637631cd2fe30
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-18 14:13:39 +0200

    Replace only list container contents on add item

--- app.py
+++ app.py
@@ -14,21 +14,19 @@ def lists():
             <ol id='first-list'>
                 <li>Item 1</li>
             </ol>
-            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
+            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container">Add Item</button>
         </div>
     """
 
 @app.route('/first-list/add', methods=['POST'])
 def newItem():
     return """
-        <div id="first-list-container">
             <h1 id="first-list-header">First List</h1>
             <ol id='first-list'>
                 <li>Item 1</li>
                 <li>Item 2</li>
             </ol>
-            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#lists">Add Item</button>
-        </div>
+            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container">Add Item</button>
     """
 
 
