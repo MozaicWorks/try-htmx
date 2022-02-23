0eead2e7e520c7887869b8ad2a3a7e5361dbbef8
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-23 13:08:17 +0200

    Refactor to use templates

--- app.py
+++ app.py
@@ -1,37 +1,17 @@
-from flask import Flask, url_for, redirect, request
+from flask import Flask, url_for, redirect, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('static', filename='index.html'))
+    return render_template("index.html")
 
 @app.route('/lists', methods=['POST'])
 def lists():
-    return """
-        <div id="first-list-container">
-            <h1 id="first-list-header">First List</h1>
-            <ol id='first-list'>
-                <li>Item 1</li>
-                <input name="items[]" type="text" value="Item 1"></input>
-            </ol>
-            <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container" hx-include="input">Add Item</button>
-        </div>
-    """
+    return render_template("lists.html")
 
 @app.route('/first-list/add', methods=['POST'])
 def newItem():
     toDoListItems = request.values.to_dict(False)['items[]']
     toDoListItems.append("New Item")
-    from jinja2 import Template
-    j2Template = Template("""
-            <h1 id="first-list-header">First List</h1>
-            <ol id='first-list'>
-            {% for item in toDoListItems %}
-                <li>{{item}}</li>
-                <input name="items[]" type="hidden" value="{{item}}"></input>
-            {% endfor %}
-            </ol>
-             <button id="add-item-first-list" hx-post="/first-list/add" hx-trigger="click" hx-target="#first-list-container" hx-include="input">Add Item</button>
-    """)
-    return j2Template.render(toDoListItems = toDoListItems)
+    return render_template('todolist.html', toDoListItems = toDoListItems)
