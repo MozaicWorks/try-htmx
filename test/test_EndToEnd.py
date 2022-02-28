77aba3089422e94964cca07d6f0449b5296c127e
Alexandru Bolboaca <alexboly@gmail.com>
2022-02-28 17:32:39 +0200

    Refactor to parameterized tests

--- test/test_EndToEnd.py
+++ test/test_EndToEnd.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from parameterized import parameterized 
 
 class EndToEndTests(unittest.TestCase):
 
@@ -13,23 +14,22 @@ class EndToEndTests(unittest.TestCase):
         self._driver.get("http://127.0.0.1:5000/")
         self._homePage = HomePage(self._driver)
 
-    def test_empty_list_created_correctly_on_first_call(self):
-        self.assertListIs(self._homePage, "First List", ["Item 1"], "Add Item")
+    @parameterized.expand([
+       ("AddNone", 0, ["Item 1"] ), 
+       ("AddOnce", 1, ["Item 1", "New Item"] ),
+       ("AddTwice", 2, ["Item 1", "New Item", "New Item"] ),
+       ("AddThreeTimes", 3, ["Item 1", "New Item", "New Item", "New Item"] ),
+   ])
+    def test_add_item_to_first_list(self, description, count, items):
+        expectedListModel = ToDoListModel("First List", items, "Add Item")
+        
+        for index in range(count):
+            self._homePage.clickAddItemToFirstList()
 
-    def test_add_item_to_first_list(self):
-        self._homePage.clickAddItemToFirstList()
-        self.assertListIs(self._homePage, "First List", ["Item 1", "New Item"], "Add Item")
-
-        self._homePage.clickAddItemToFirstList()
-        self.assertListIs(self._homePage, "First List", ["Item 1", "New Item", "New Item"], "Add Item")
+        self.assertEqual(self._homePage.toListModel(), expectedListModel)
 
     def tearDown(self):
         self._driver.close()
-
-    def assertListIs(self, homePage, listName, listItemsTexts, addItemLabel):
-        self.assertEqual(listName, homePage.firstListHeader().text)
-        self.assertListEqual(homePage.firstListItemsTexts(), listItemsTexts)
-        self.assertEqual(addItemLabel, homePage.firstListAddItemButton().text)
 
 class HomePage:
     def __init__(self, driver):
@@ -49,6 +49,28 @@ class HomePage:
 
     def clickAddItemToFirstList(self):
         self.firstListAddItemButton().click()
+
+    def toListModel(self):
+        return ToDoListModel(self.firstListHeader().text, self.firstListItemsTexts(), self.firstListAddItemButton().text)
+
+class ToDoListModel:
+    _name: str
+    _items: list
+    _addLabel: str
+    
+    def __init__(self, name, items, addLabel):
+        self._name = name
+        self._items = items
+        self._addLabel = addLabel
+
+    def __eq__(self, other):
+        return self._name == other._name and \
+                self._items == other._items and \
+                self._addLabel == other._addLabel
+
+    def __repr__(self):
+        return f"ToDoListModel(name={repr(self._name)}, items={repr(self._items)}, addLabel ={repr(self._addLabel)})"
+
 
 if __name__ == '__main__':
     unittest.main()
